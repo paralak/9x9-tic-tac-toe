1942680b2c6b6d33aec0a349254d2a86e186e1d6
paralak <paralakmc@gmail.com>
2024-04-09 21:54:21 +0300

    0.1

---
+++ cls/tic_tac_toe.js
@@ -0,0 +1,112 @@
+
+const TicTacToe = (()=>{
+
+  const shadowDOM = //html
+  `
+    <style>
+      * {
+        margin:0;
+        padding:0;
+        box-sizing:border-box;
+      }
+      td {
+        min-height:30px;
+        min-width:30px;
+        display:inline-block;
+        padding:3px 7px 2px;
+        font-size:20px;
+      }
+      td:nth-child(-n + 2) {
+        border-right:1px solid black;
+      }
+      tr {
+        min-height:30px;
+        display:block;
+      }
+      tr:nth-child(-n + 2) {
+        border-bottom:1px solid black;
+      }
+      .ready {
+        background:green;
+      }
+    </style>
+    <table>
+      <tr><td id="o11"></td><td id="o12"></td><td id="o13"></td></tr>
+      <tr><td id="o21"></td><td id="o22"></td><td id="o23"></td></tr>
+      <tr><td id="o31"></td><td id="o32"></td><td id="o33"></td></tr>
+    </table>
+  `;
+
+  const winPatterns = [
+    '000102',
+    '101112',
+    '202122',
+    '001020',
+    '011121',
+    '021222',
+    '001122',
+    '021120',
+  ]
+
+  class TicTacToe extends HTMLElement {    
+    connectedCallback() {
+      const shadow = this.attachShadow({mode: 'open'});
+      shadow.innerHTML = shadowDOM;
+      this.field = [[document.createElement("td")],[],[]];
+      for (let i = 1; i<4; ++i) {
+        for (let j = 1; j<4; ++j) {
+          this.field[i-1][j-1] = shadow.querySelector(`#o${i}${j}`);
+        }
+      }
+      this.status = this.statuses.inProgress;
+      const turn = ((el)=>{return function  () {
+        if (el.status == el.statuses.inProgressTurn && this.innerText == "") {
+          this.innerText = el.now_turn;
+          el.update();
+          el.turn_callback(this.id[1]-1, this.id[2]-1);
+        }
+      }})(this);
+      for (let i = 0; i < 3; ++i) for (let j = 0; j < 3; ++j) this.field[i][j].addEventListener('click', turn);
+    }
+
+    waitTurn(now_turn) {
+      if (this.status == this.statuses.inProgress) {
+        this.status = this.statuses.inProgressTurn;
+        this.now_turn = now_turn;
+        for (let i = 0; i < 3; ++i) for (let j = 0; j < 3; ++j) {
+          if (this.field[i][j].innerText == "") this.field[i][j].classList.add("ready");
+        }
+        return 0;
+      } else {
+        return -1;
+      }
+    }
+    
+    unWaitTurn() {
+      if (this.status == this.statuses.inProgressTurn) {
+        for (let i = 0; i < 3; ++i) for (let j = 0; j < 3; ++j) this.field[i][j].classList.remove('ready');
+        this.status = this.statuses.inProgress;
+      }
+    }
+
+    update() {
+      
+    }
+  };
+
+  TicTacToe.prototype.field = [[document.createElement("td")]];
+  TicTacToe.prototype.statuses = {
+    inProgress:0,
+    winX:1,
+    winY:2,
+    tie:3,
+    inProgressTurn:4,
+  }
+  TicTacToe.prototype.status = TicTacToe.prototype.statuses.inProgress;
+  TicTacToe.prototype.now_turn = "";
+  TicTacToe.prototype.turn_callback = ()=>{};
+
+  return TicTacToe;
+})();
+
+customElements.define('tic-tac-toe', TicTacToe);
---
+++ cls/tic_tac_toe_9x9.js
@@ -0,0 +1,85 @@
+const TicTacToe9x9 = (()=>{
+
+  const shadowDOM = //html
+  `
+    <style>
+      * {
+        margin:0;
+        padding:0;
+        box-sizing:border-box;
+      }
+      td {
+        min-height:30px;
+        min-width:30px;
+        padding:10px;
+        display:inline-block;
+      }
+      td:nth-child(-n + 2) {
+        border-right:2px solid black;
+      }
+      tr {
+        min-height:30px;
+        display:block;
+      }
+      tr:nth-child(-n + 2) {
+        border-bottom:2px solid black;
+      }
+    </style>
+    <table>
+      <tr>
+        <td id="o11"><tic-tac-toe></tic-tac-toe></td>
+        <td id="o12"><tic-tac-toe></tic-tac-toe></td>
+        <td id="o13"><tic-tac-toe></tic-tac-toe></td>
+      </tr>
+      <tr>
+        <td id="o21"><tic-tac-toe></tic-tac-toe></td>
+        <td id="o22"><tic-tac-toe></tic-tac-toe></td>
+        <td id="o23"><tic-tac-toe></tic-tac-toe></td>
+      </tr>
+      <tr>
+        <td id="o31"><tic-tac-toe></tic-tac-toe></td>
+        <td id="o32"><tic-tac-toe></tic-tac-toe></td>
+        <td id="o33"><tic-tac-toe></tic-tac-toe></td>
+      </tr>
+    </table>
+  `;
+
+  class TicTacToe9x9 extends HTMLElement {
+    
+    connectedCallback() {
+      const shadow = this.attachShadow({mode: 'open'});
+      shadow.innerHTML = shadowDOM;
+      this.field = [[new TicTacToe()],[],[]];
+      for (let i = 1; i<4; ++i) {
+        for (let j = 1; j<4; ++j) {
+          this.field[i-1][j-1] = shadow.querySelector(`#o${i}${j} tic-tac-toe`);
+        }
+      }
+      this.bindTurns();
+    }
+
+    bindTurns() {
+      const turn = ((el)=>{return function (x,y) {
+        for (let i = 0; i < 3; ++i) for (let j = 0; j < 3; ++j) el.field[i][j].unWaitTurn();
+        if (el.field[x][y].waitTurn(el.turn)) for (let i = 0; i < 3; ++i) for (let j = 0; j < 3; ++j) el.field[i][j].waitTurn(el.turn);;
+        el.turn = el.turn == "X" ? "O" : "X";
+        el.update();
+      }})(this);
+      for (let i = 0; i < 3; ++i) for (let j = 0; j < 3; ++j) {
+        this.field[i][j].turn_callback = turn;
+        this.field[i][j].waitTurn(this.turn);
+      }
+    }
+
+    update() {
+
+    }
+  };
+
+  TicTacToe9x9.prototype.field = [[new TicTacToe()]]
+  TicTacToe9x9.prototype.turn = "X";
+
+  return TicTacToe9x9;
+})();
+
+customElements.define('tic-tac-toe-9x9', TicTacToe9x9);
